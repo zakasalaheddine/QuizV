e2dca59e1c584938f759d7c2f40121ee97528190
Zaka Salah Eddine <zakasalaheddine@gmail.com>
2020-10-16 09:19:47 +0100

    feat: update all the single quiz logic to it's own Hook

---
+++ hooks/useSingleQuizHandler.js
@@ -0,0 +1,55 @@
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
+
+import { setDataToAnswer } from "context/AnswerQuizActions";
+import { changeSelectedLang } from "context/CreateQuizActions";
+import AnswerQuizContext from "context/AnswerQuizContext";
+import CreateQuizContext from "context/CreateQuizContext";
+
+import QuizDashboard from "components/QuizDashboard";
+import UserQuiz from "components/UserQuiz";
+import Results from "components/UserQuiz/Results";
+
+export const useSingleQuizHandler = (quizData, slug) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [quizIsMine, setQuizIsMine] = useState(false);
+  const [alreadyAnswerd, setAlreadyAnswerd] = useState(false);
+  const [answerState, dispatchAnswer] = useContext(AnswerQuizContext);
+  const [_, dispatch] = useContext(CreateQuizContext);
+
+  const checkIfQuizIsMine = (slug) => {
+    const localSlug = localStorage.getItem("QUIZV_SLUG");
+    if (localSlug && localSlug === slug) return true;
+    return false;
+  };
+  const checkIfQuizAlreadyAnswerd = (slug) => {
+    const answers = localStorage.getItem(slug);
+    if (answers) return true;
+    return false;
+  };
+  useEffect(() => {
+    if (!quizData) {
+      router.push("/");
+      return;
+    }
+    if (checkIfQuizIsMine(slug)) {
+      setQuizIsMine(true);
+      dispatch(changeSelectedLang(quizData.lang));
+    } else {
+      dispatchAnswer(setDataToAnswer(quizData));
+      setQuizIsMine(false);
+      if (checkIfQuizAlreadyAnswerd(slug)) setAlreadyAnswerd(true);
+    }
+    setLoading(false);
+  }, []);
+
+  const renderCorrectComponent = () => {
+    if (loading) return <p>Loading ....</p>;
+    if (quizIsMine) return <QuizDashboard slug={slug} quizData={quizData} />;
+    if (alreadyAnswerd) return <Results />;
+    return <UserQuiz creator={answerState.creatorName} />;
+  };
+
+  return { renderCorrectComponent };
+};
--- pages/[quiz]/index.jsx
+++ pages/[quiz]/index.jsx
@@ -1,50 +1,9 @@
-import { useEffect, useState, useContext } from "react"
-import { useRouter } from "next/router";
-import QuizDashboard from "components/QuizDashboard";
-import UserQuiz from "components/UserQuiz";
 import Axios from "axios";
-import AnswerQuizContext from "context/AnswerQuizContext";
-import { setDataToAnswer } from "context/AnswerQuizActions";
-import Results from "components/UserQuiz/Results";
-import CreateQuizContext from "context/CreateQuizContext";
-import { changeSelectedLang } from "context/CreateQuizActions";
-import Logo from "components/Logo";
-import { AppContainer } from "../../components/Shared/AppContainer";
+import { AppContainer } from "components/Shared/AppContainer";
+import { useSingleQuizHandler } from "../../hooks/useSingleQuizHandler";
 
 export default function QuizPage({ slug, data }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true)
-  const [quizIsMine, setQuizIsMine] = useState(false)
-  const [alreadyAnswerd, setAlreadyAnswerd] = useState(false)
-  const [answerState, dispatchAnswer] = useContext(AnswerQuizContext)
-  const [_, dispatch] = useContext(CreateQuizContext)
-  useEffect(() => {
-    if (!data) {
-      router.push("/")
-      return;
-    }
-    const localSlug = localStorage.getItem("QUIZV_SLUG")
-    if (localSlug && localSlug === slug) {
-      setQuizIsMine(true)
-      dispatch(changeSelectedLang(data.lang))
-    } else {
-      const AnswersData = localStorage.getItem(slug)
-      if (AnswersData) {
-        setAlreadyAnswerd(true)
-      }
-      dispatchAnswer(setDataToAnswer(data))
-      setQuizIsMine(false)
-    }
-    setLoading(false)
-  }, [])
-
-  const renderCorrectComponent = () => {
-    if (loading) return <p>Loading ....</p>
-    if (quizIsMine) return <QuizDashboard slug={slug} quizData={data} />
-    if (alreadyAnswerd) return <Results />
-    return <UserQuiz creator={answerState.creatorName} />
-  }
-
+  const { renderCorrectComponent } = useSingleQuizHandler(data, slug)
   return (
     <AppContainer>
       {renderCorrectComponent()}
